13137bed58dc7e3a58bd8bb50078ce156bff5606
ganeshbarve88 <30840789+ganeshbarve88@users.noreply.github.com>
2017-09-06 22:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,13 +105,13 @@ app.get('/:articleName', function(req, res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function(req,res){
+/*app.get('/article-two', function(req,res){
     res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three', function(req,res){
     res.send(createTemplate(articleThree));
-});
+});*/
 
 
 app.get('/ui/style.css', function (req, res) {
