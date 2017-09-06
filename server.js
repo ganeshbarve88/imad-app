8496afcd703b1ab9692ae81f32c4a34d0f4bbafa
ganeshbarve88 <30840789+ganeshbarve88@users.noreply.github.com>
2017-09-06 22:32:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    'article-one': {
+    'articleone': {
     title : 'Article one|Ganesh Barve',
     date : 'Aug 29th, 2017',
     heading :'Article-one',
